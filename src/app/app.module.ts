3785d439efc4373d597fd573664eb74bb36b54eb
Guillermo Valarezo Guzman <gvalarezog@gmail.com>
2023-04-25 15:26:07 -0500

    remove layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './layout/home/home.component';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { ToolbarComponent } from './layout/toolbar/toolbar.component';
-import { PageWrapperComponent } from './layout/page-wrapper/page-wrapper.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardModule } from './dashboard/dashboard.module';
 import { AbmAlumnosModule } from './dashboard/pages/alumnos/abm-alumnos/abm-alumnos.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    SidebarComponent,
-    ToolbarComponent,
-    PageWrapperComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AbmAlumnosModule,
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { CardsModule } from '../layout/pages/cards/cards.module';
-import { ButtonsAndInconsModule } from '../layout/pages/buttons-and-incons/buttons-and-incons.module';
 import { DirectivesModule } from 'src/app/shared/directives/directives.module';
-import { AlumnosModule } from './pages/alumnos/alumnos.module';
-import { CommonModule } from '@angular/common';
 import { AbmAlumnosModule } from './pages/alumnos/abm-alumnos/abm-alumnos.module';
+import { CommonModule } from '@angular/common';
+import { AlumnosModule } from './pages/alumnos/alumnos.module';
 
 @NgModule({
   declarations: [DashboardComponent],
@@ -20,8 +18,6 @@ import { AbmAlumnosModule } from './pages/alumnos/abm-alumnos/abm-alumnos.module
     MatIconModule,
     MatButtonModule,
     AbmAlumnosModule,
-    CardsModule,
-    ButtonsAndInconsModule,
     AlumnosModule,
     DirectivesModule,
   ],
